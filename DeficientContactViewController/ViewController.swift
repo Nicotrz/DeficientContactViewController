254be310e3d75964fbcaf761c2239f2467e3bb71
Nicotrz <nicolas.trz@gmail.com>
2019-10-03 08:16:51 +0200

    Initial Commit

---
+++ DeficientContactViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeficientContactViewController
+//
+//  Created by Nicolas Sommereijns on 03/10/2019.
+//  Copyright © 2019 Nicolas Sommereijns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
